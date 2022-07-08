dca7b069e1146a1e535ae5d67d862d48b6bd9610
Dac <dacoria@gmail.com>
2022-07-08 20:19:18 +0200

    fix bg

---
+++ LoveLetter/Assets/BgScriptInLeftBottomEditMode.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class BgScriptInLeftBottomEditMode : MonoBehaviour
+{
+    public Image Image;
+
+    void Update()
+    {
+        if (EditorApplication.isPlaying) return;
+
+        var leftLimit = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        var rightLimit = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        var downLimit = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        var topLimit = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+        Image.transform.position = new Vector3(leftLimit, downLimit);
+    }
+}
---
+++ LoveLetter/Assets/BgScriptMoveToRight.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BgScriptMoveToRight : MonoBehaviour
+{
+    public Image Image;
+    public GameObject BgPrefab;
+
+    private float speed = 1.5f;
+    public bool FirstGo = false;
+    private bool spawnedNewGo = false;
+
+    private void Update()
+    {
+        transform.position += new Vector3(1,0,0) * speed * Time.deltaTime;
+
+        if(!FirstGo && transform.position.x > -15 && !spawnedNewGo)
+        {
+            SpawnNewBg();
+        }
+        if (transform.position.x > 100)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (FirstGo)
+        {
+            SpawnNewBg();
+        }
+
+    }
+
+    private void SpawnNewBg()
+    {
+        if(spawnedNewGo)
+        {
+            return;
+        }
+
+        var newBg = Instantiate(BgPrefab, transform.parent.transform);
+        newBg.transform.position = transform.position - new Vector3(transform.GetComponent<RectTransform>().sizeDelta.x, 0, 0);
+        newBg.GetComponent<BgScriptMoveToRight>().FirstGo = false;
+        spawnedNewGo = true;
+    }
+}
